9aa8aebd28fce61534e0f2d640b7b6df213ef9e8
Vitaliy Vanchak <vvanchak2@gmail.com>
2018-12-20 15:34:28 +0200

    Created Upload service

--- src/app/routes/player-route/components/file-upload/file-upload.component.ts
+++ src/app/routes/player-route/components/file-upload/file-upload.component.ts
@@ -1,15 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { AngularFireStorage, AngularFireUploadTask } from '@angular/fire/storage';
 import { AngularFirestore } from '@angular/fire/firestore';
-import { auth } from 'firebase/app';
-import { Observable } from 'rxjs';
-import { tap, finalize } from 'rxjs/operators';
-import { utf8Encode } from '@angular/compiler/src/util';
 
-const getUserUID = () => {
-  const user = auth().currentUser
-  return user.uid;
-}
+import { Observable } from 'rxjs';
+import { UploadService } from '../../services/upload.service'
 
 interface upload {
   name: string,
@@ -24,14 +18,17 @@ interface upload {
   styleUrls: ['./file-upload.component.scss']
 })
 export class FileUploadComponent {
-  // State for dropzone CSS toggling
   isHovering: boolean;
-  downloadURL:Observable<any>;
+  //downloadURL:Observable<any>;
   upload: Array<upload> = [];
-  uid: string = getUserUID();
+  
   isUploadListHidden: boolean = true;
 
-  constructor(private storage: AngularFireStorage, private db: AngularFirestore) { }
+  constructor(
+    private storage: AngularFireStorage, 
+    private db: AngularFirestore,
+    private uploadService: UploadService
+  ) { }
 
   
   toggleHover(event: boolean) {
@@ -44,7 +41,6 @@ export class FileUploadComponent {
     this.isUploadListHidden = false;
     Array.from(event).forEach( file => new Promise ((resolve, reject) => {
       console.log(file);
-      //const cr = btoa(file.name)
       this.upload[index] = { 
         name: file.name
       }
@@ -56,45 +52,21 @@ export class FileUploadComponent {
 
   startUpload(file: File, index: number) {
     console.log(index);
-    // The File object
-    //const file = event.item(0)
-    // Client-side validation example
     if (file.type.split('/')[0] !== 'audio') { 
       console.error('unsupported file type :( ')
       return;
     }
-    // The storage path
-    const path = `audio/${new Date().getTime()}${this.uid}_${file.name}`;
-
-    // Totally optional metadata
-    const customMetadata = { app: 'Echo - audio player project' };
-
-    // The main task
-    const task = this.storage.upload(path, file, { customMetadata })
-
-    // Progress monitoring
-    let isUploaded = false;
+    
     const previousValue = this.upload[index];
+    const uploadData = this.uploadService.uploadAudioFile(file)
+
     this.upload[index] = {
       ...previousValue,
-      task, 
-      percentage: task.percentageChanges(),
-      snapshot: task.snapshotChanges().pipe(
-        tap(snap => {
-          //console.log(snap)
-          if (snap.bytesTransferred === snap.totalBytes && !isUploaded) {
-            isUploaded = true;
-            // Update firestore on completion
-            this.db.collection(this.uid).add( { path, size: snap.totalBytes })
-            console.log('file uploaded');
-          }
-        })
-      )
-    };
-    // The file's download URL
-    this.upload[index].snapshot.pipe(finalize(() => this.downloadURL = this.storage.ref(path).getDownloadURL())).subscribe();
+      ...uploadData
+    }; 
+    //this.upload[index].snapshot.pipe(finalize(() => this.downloadURL = this.storage.ref(path).getDownloadURL())).subscribe();
   }
-  // Determines if the upload task is active
+
   isActive(snapshot) {
     return snapshot.state === 'running' && snapshot.bytesTransferred < snapshot.totalBytes
   }
---
+++ src/app/routes/player-route/services/upload.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import { AngularFireStorage, AngularFireUploadTask } from '@angular/fire/storage';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { auth } from 'firebase/app';
+import { tap, finalize } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UploadService {
+  uid: string;
+  constructor(
+    private storage: AngularFireStorage,
+    private db: AngularFirestore
+  ) {
+    const user = auth().currentUser;
+    this.uid = user.uid;
+  }
+
+  uploadAudioFile(file: File) {
+    const path = `audio/${new Date().getTime()}${this.uid}_${file.name}`;
+    const customMetadata = { app: 'Echo - audio player project' };
+    const task = this.storage.upload(path, file, { customMetadata })
+    let isUploaded = false;
+    return {
+      task, 
+      percentage: task.percentageChanges(),
+      snapshot: task.snapshotChanges().pipe(
+        tap(snap => {
+          if (snap.bytesTransferred === snap.totalBytes && !isUploaded) {
+            isUploaded = true;
+            this.db.collection(this.uid).add( { path, size: snap.totalBytes })
+            console.log('file uploaded');
+          }
+        })
+      )
+    }
+    
+  }
+}
